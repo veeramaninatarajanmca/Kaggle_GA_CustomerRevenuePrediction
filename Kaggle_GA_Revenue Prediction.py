4142c389804d75197d01f22fba39b5b818d12283
Veeramani Natarajan <veeramaninatarajanmca@gmail.com>
2018-11-10 09:46:48 +0530

    First version Kaggle Competition
    First version

---
+++ Kaggle_GA_Revenue Prediction.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 14 20:31:33 2018
+
+@author: Veeramani Natarajan
+@Title: Kaggle Google Analytics - Customer Revenue Prediction
+
+"""
+## Libraries
+import pandas as pd
+
+## Data pre-processing
+ga_df=pd.read_csv("D:\Dropbox\Hackathons\kaggle\Google Analytics Customer Revenue Prediction_All\train.csv")
+
+
